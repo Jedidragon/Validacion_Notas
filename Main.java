b75df9f34f5437304d983ca1189d6e059d51b7aa
Jose Manuel <josemanuel.pla@gmail.com>
2018-05-13 12:41:14 +0200

    Version con javadoc del foncinamiento

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ package com.jetbrains;
 
 public class Main {
 
+    /**
+     *
+     * @param args son los parametros de argumento que hemos introdicido en las posiciones [0],[1],[2] del main
+     */
     public static void main(String[] args) {
-
 
             String valor1;
             String valor2;
@@ -17,6 +20,12 @@ public class Main {
 
         }
 
+    /**
+     *
+     * @param valor1 es la variable que devulelve el valor de la notas a conseguir
+     * @param valor2 es la variable que devulelve el valor de la notas  conseguir
+     * @return devuelve el resultado obtenido de las variables valor1 y valor2
+     */
             private static String veredicto(String valor1, String valor2) {
                 if (valor1.equals(valor2)) {
                     return "Perfecto. Has tenido buen criterio.";
